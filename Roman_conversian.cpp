54bbb066e10945dedafd29141744647e5d4a1606
Muhammad Zain Ali <111534311+zainaliqureshi174@users.noreply.github.com>
2023-08-27 09:54:53 +0500

    Add files via upload

---
+++ Roman_conversian.cpp
@@ -0,0 +1,39 @@
+//Write a program that ask the user to enter a number in the range of 1—10
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a number in range of 1 - 10: ";
+    cin >> a;
+    if (a==1){
+        cout <<"I";
+    }
+    else if (a==2){ 
+        cout <<"II";
+    }
+    else if (a==3){
+        cout <<"III";
+    }
+    else if (a==4){
+        cout <<"IV";
+    }
+    else if (a==5){
+        cout <<"V";
+    }
+    else if (a==6){
+        cout <<"VI";
+    }
+    else if (a==7){
+        cout <<"VII";
+    }
+    else if (a==8){
+        cout <<"VIII";
+    }
+    else if (a==9){
+        cout <<"IX";
+    }
+    else 
+    cout <<"X";
+    
+    return 0;
+}
